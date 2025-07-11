a67103a8662f9428ee5f7275276b477a3e932fb8
Daniel Marandici <danielmarandici2001@gmail.com>
2025-07-11 15:02:01 +0300

    Added example to invoke stored procedures

---
+++ src/main/java/org/example/snowflakejava/controller/UserRequestController.java
@@ -0,0 +1,104 @@
+package org.example.snowflakejava.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import org.example.snowflakejava.controller.dto.CreateUserRequestDto;
+import org.example.snowflakejava.repository.UserRequestRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user-request")
+public class UserRequestController {
+
+    private final UserRequestRepository userRequestRepository;
+
+    public UserRequestController(UserRequestRepository userRequestRepository) {
+        this.userRequestRepository = userRequestRepository;
+    }
+
+    @Operation(
+            summary = "Invoke string procedure with string as return",
+            description = """
+                    
+                    """)
+    @GetMapping("/call-string-stored-procedure")
+    public ResponseEntity<UUID> callString(@RequestParam String value) {
+        UUID response = userRequestRepository.callString(value);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/call-int-array-stored-procedure")
+    public ResponseEntity<UUID> callIntArrayStoredProcedure(@RequestParam int[] value) {
+        UUID response = userRequestRepository.callIntArrayStoredProcedure(value);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/call-int-array-sql")
+    public ResponseEntity<UUID> callIntArraySql(@RequestParam int[] value) {
+        UUID response = userRequestRepository.callIntArraySql(value);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Save userRequest using stored procedure",
+            description = """
+                    Json for testing:
+                    ```
+                    {
+                      "reason": "CancelledPlans",
+                      "allocationPeriodType": "Quartely",
+                      "sourceIdYears": [
+                        {
+                          "year": "2021",
+                          "source": "Radio"
+                        },
+                        {
+                          "year": "2022",
+                          "source": "TV"
+                        }
+                      ],
+                      "months": [
+                        0,1,2,3,4
+                      ]
+                    }
+                    ```
+                    """)
+    @PostMapping("/create-using-stored-procedure")
+    public ResponseEntity<UUID> createUserRequestStoredProcedure(@RequestBody CreateUserRequestDto dto) throws JsonProcessingException {
+    UUID response = userRequestRepository.createUserRequestStoredProcedure(dto.reason(), dto.allocationPeriodType(), dto.sourceIdYears(), dto.months());
+    return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Save userRequest using sql",
+            description = """
+                    Json for testing:
+                    ```
+                    {
+                      "reason": "CancelledPlans",
+                      "allocationPeriodType": "Quartely",
+                      "sourceIdYears": [
+                        {
+                          "year": "2021",
+                          "source": "Radio"
+                        },
+                        {
+                          "year": "2022",
+                          "source": "TV"
+                        }
+                      ],
+                      "months": [
+                        0,1,2,3,4
+                      ]
+                    }
+                    ```
+                    """)
+    @PostMapping("/create-using-sql")
+    public ResponseEntity<UUID> createUserRequestSql(@RequestBody CreateUserRequestDto dto) throws JsonProcessingException {
+        UUID response = userRequestRepository.createUserRequestSql(dto.reason(), dto.allocationPeriodType(), dto.sourceIdYears(), dto.months());
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/org/example/snowflakejava/controller/dto/SourceAndYearDto.java
@@ -0,0 +1,6 @@
+package org.example.snowflakejava.controller.dto;
+
+public record SourceAndYearDto(
+        String year,
+        String source
+){}
